69555076223c8d73128512810eaf327d78f868da
Pawan <pawankumarswami232@gmail.com>
2021-11-12 23:15:41 -0800

    Drawing_on_canvas

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kids Drawing-App
+
+Code-along project from the Udemy-course [The Complete Android 10 & Kotlin Development Masterclass](https://www.udemy.com/course/android-kotlin-developer/) by Denis Panjuta.
+
+<div style="text-align: center">
+  <img src="./images/image1.png" alt="screenshot 1" width="300" />&nbsp;&nbsp; 
+  <img src="./images/image2.png" alt="screenshot 2" width="300" />&nbsp;&nbsp;
+  <img src="./images/image3.png" alt="screenshot 3" width="300" />&nbsp;&nbsp;
+  <img src="./images/image4.png" alt="screenshot 4" width="300" />
+</div>
+
---
+++ app/src/main/java/com/mizech/drawingapp/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.mizech.drawingapp
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
+
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+
+        ib_gallery.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(pickPhotoIntent, GALLERY)
+            } else {
+                requestStoragePermission()
+            }
+        }
+
+        ib_undo.setOnClickListener {
+            drawing_view.onClickUndo()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                try {
+                    if (data!!.data != null) {
+                        iv_background.visibility = View.VISIBLE
+                        iv_background.setImageURI(data.data)
+                    } else {
+                        Toast.makeText(this,
+                            "Error occurred while trying to add the immage",
+                            Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Choose a brush size!")
+        val smallButton = brushDialog.ib_small_brush
+        val mediumButton = brushDialog.ib_medium_brush
+        val largeButton = brushDialog.ib_large_brush
+
+        smallButton.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumButton.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeButton.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
+    }
+
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
+            val imageButton = view as ImageButton
+
+            val colorTag = imageButton.tag.toString()
+            drawing_view.setColor(colorTag)
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+            mImageButtonCurrentPaint = view
+        }
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())) {
+            Toast.makeText(this, "You need permission to add a background.",
+                Toast.LENGTH_LONG).show()
+        }
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this,
+                    "Permission added. Now you can add a background-file.",
+                    Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,
+                    "Adding permission became denied!",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat
+            .checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
+    }
+}
